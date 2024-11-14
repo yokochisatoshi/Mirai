07facdacca84d15539fa1b8684a216c99acc4b56
Sumono-code <kakutoumono@gmail.com>
2024-11-14 23:38:18 +0900

    Update GUI.cs

--- Assets/Script/GUI.cs
+++ Assets/Script/GUI.cs
@@ -1,20 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
 public class GUI : MonoBehaviour
 {
+    private Camera mainCamera;
+    private Vector3 currentPosition = Vector3.zero;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            var raycastHitList = Physics.RaycastAll(ray).ToList();
+            if (raycastHitList.Any())
+            {
+                var distance = Vector3.Distance(mainCamera.transform.position, raycastHitList.First().point);
+                var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+
+                currentPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+                currentPosition.y = 0;
+            }
+        }
+
     }
+
+    void OnDrawGizmos()
+    {
+        if (currentPosition != Vector3.zero)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(currentPosition, 0.5f);
+        }
+    }
+
 
     public void MouseDownUI()
     {
