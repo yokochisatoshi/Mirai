846c03d699ce57e6e7a1fa647cb6e38babfba7bb
yokochisatoshi <satoyoko315@icloud.com>
2024-12-12 15:19:52 +0900

    12/12_cameraMove

---
+++ Assets/Script/yokochi/camera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public float moveVal = 5.0f;    // カメラのスピード
+    bool ableMove;                  // カメラ移動可能フラグ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ableMove = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (ableMove == true)
+        {
+            Vector3 pos = transform.position;
+            float val = Input.GetAxis("Mouse ScrollWheel");
+            pos.x += val * moveVal;
+            transform.position = pos;
+        }
+    }
+}
