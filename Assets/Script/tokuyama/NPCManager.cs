d15674bcc02f48999d3194b71eace508907dc80c
Sumono-code <kakutoumono@gmail.com>
2024-12-28 21:31:46 +0900

    ゾエさんにトクヤマの統合

--- Assets/Script/tokuyama/NPCManager.cs
+++ Assets/Script/tokuyama/NPCManager.cs
@@ -9,37 +9,52 @@ public class NPCManager : MonoBehaviour
     private int nInterval = 60;  //ステージなどによってPopのインターバルは変わるのでそれ用
     private int nCntInterval = 0;  //インターバルカウント用
 
-    
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         //地面の手前の端と奥の端の座標 X座標です
-    int StartPos = 0;
-    //int EndPos = 100;
+        int StartPos = 0;
+        //int EndPos = 100;
 
-    nCntInterval++;
+
+        if ((PlayerData.Instance.nTime >= 60.0f && PlayerData.Instance.nTime <= 120.0f) ||        //モーニング
+             (PlayerData.Instance.nTime >= 360.0f && PlayerData.Instance.nTime <= 420.0f) ||        //昼
+             (PlayerData.Instance.nTime >= 720.0f && PlayerData.Instance.nTime <= 780.0f))          //夜
+        {
+            nInterval = 15;
+        }
+        else
+        {
+            nInterval = 60;
+        }
+
+
+        nCntInterval++;
         //ステージによって場合分け
         switch (PlayerData.Instance.nCurrentStage)
         {
             case 0:
-                
-                if(nCntInterval>nInterval)
+
+
+
+                if (nCntInterval > nInterval)
                 {
                     nCntInterval = 0;
-                    Instantiate(NPC_00, new Vector3(StartPos, 0, Random.Range(0.0f, 10.0f)), Quaternion.Euler(0, 90, 0));
-                  //  Debug.Log("tt");
+                    Instantiate(NPC_00, new Vector3(StartPos, 0, Random.Range(-1.0f, 11.0f)), Quaternion.Euler(0, 90, 0));
+
                 }
                 break;
 
         }
-        
+
     }
 }
